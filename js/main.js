5c2d7c69bf5c9f7c652fb7c734b75b946f4f76ce
Ahmed <sanada927@gmail.com>
2024-06-21 17:14:02 +0300

    update JS file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-var cards = document.querySelectorAll('.container .row .item');
-var lightBox = document.querySelector('.lightBox');
-var closeBtn = document.querySelector('#closeBtn');
-var rightArrow = document.querySelector('#rightImage');
-var leftArrow = document.querySelector('#leftImage');
+const cards = document.querySelectorAll('.container .row .item');
+const lightBox = document.querySelector('.lightBox');
+const closeBtn = document.querySelector('#closeBtn');
+const rightArrow = document.querySelector('#rightImage');
+const leftArrow = document.querySelector('#leftImage');
 
-var currentCard;
+let currentCard;
 
-for(var i = 0 ; i < cards.length ; i++){
+for(let i = 0 ; i < cards.length ; i++){
     cards[i].addEventListener('click',function(e){
-        currentCard = this; // => return the whole div because of the event bubbling
-        // currentCard = e.target; // => return the image itself which is the element clicked // BUT to work with it then we need to search image index which is poor for the performance compared to the current solution
+        currentCard = i;
         lightBox.children[0].setAttribute('src',
             this.children[0].children[0].getAttribute('src'));
 
@@ -31,7 +30,7 @@ lightBox.parentElement.addEventListener('click',function(e){
 lightBox.addEventListener('click',function(e){
     e.stopPropagation();
 });
-var scale = 1.5;
+let scale = 1.5;
 lightBox.children[0].addEventListener('dblclick',function(e){
     lightBox.style.transform = `scale(${scale})`;
     scale = (scale === 1 ? 1.5 : 1);
@@ -49,20 +48,20 @@ rightArrow.addEventListener('click',function(e){
 
 function slide(step){
     if(step == -1){ // to left
-        if(currentCard == cards[0]){
-            currentCard = cards[cards.length-1];
+        if(currentCard == 0){
+            currentCard = cards.length-1;
         }
-        else {currentCard = currentCard.previousElementSibling;}
+        else {currentCard--;}
     }
     else{ // to right
-        if(currentCard == cards[cards.length-1]){
-            currentCard = cards[0];
+        if(currentCard == cards.length-1){
+            currentCard = 0;
         }
-        else {currentCard = currentCard.nextElementSibling;}
+        else {currentCard++;}
     }
     lightBox.children[0].setAttribute('src',
-        currentCard.children[0].children[0].getAttribute('src'));
+        cards[currentCard].children[0].children[0].getAttribute('src'));
 
     lightBox.children[0].setAttribute('alt',
-        currentCard.children[0].children[0].getAttribute('alt'));
+        cards[currentCard].children[0].children[0].getAttribute('alt'));
 }
